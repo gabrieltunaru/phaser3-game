66a9940b7e03db28630bfddbc9c6d4228d424201
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2020-05-18 15:59:53 +0300

    added laser

--- public/index.js
+++ public/index.js
@@ -1,148 +1,85 @@
 var config = {
-  type: Phaser.WEBGL,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
+    type: Phaser.WEBGL,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {y: 0},
+            debug: false,
+        },
     },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("ground", "assets/platform.png");
-  this.load.image("star", "assets/star.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dude.png", {
-    frameWidth: 32,
-    frameHeight: 48,
-  });
+    this.load.image("star", "assets/star16.png");
+    this.load.image("ship", "assets/spaceship.png");
+    this.load.image("laser", "assets/laser.png");
 }
-
-var platforms;
-var score = 0;
-var scoreText;
 
 function create() {
-  this.add.image(400, 300, "sky");
+    stars = [];
+    for (let i = 0; i < 10; i++) {
+        stars.push(
+            this.physics.add.group({
+                key: "star",
+                repeat: 22,
+                setXY: {x: 12, y: i * 100, stepX: 70},
+                setCollideWorldBounds: true,
+            })
+        );
+    }
 
-  platforms = this.physics.add.staticGroup();
-
-  platforms.create(400, 568, "ground").setScale(2).refreshBody();
-
-  platforms.create(600, 400, "ground");
-  platforms.create(50, 250, "ground");
-  platforms.create(750, 220, "ground");
-
-  player = this.physics.add.sprite(100, 450, "dude");
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.physics.add.collider(player, platforms);
-
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude", frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  stars = this.physics.add.group({
-    key: "star",
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 },
-  });
-
-  stars.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
-
-  this.physics.add.collider(stars, platforms);
-  this.physics.add.overlap(player, stars, collectStar, null, this);
-  scoreText = this.add.text(16, 16, "score: 0", {
-    fontSize: "32px",
-    fill: "#000",
-  });
-  bombs = this.physics.add.group();
-
-  this.physics.add.collider(bombs, platforms);
-
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
+    ship = this.physics.add.sprite(800, 600, "ship");
+    console.log(ship);
+    cursors = this.input.keyboard.createCursorKeys();
 }
+
+let lasers = 0;
+
 
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play("right", true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play("turn");
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-  }
+    moveShip();
+    if (cursors.space.isDown && lasers === 0) {
+        lasers += 1
+        this.laser = this.physics.add.sprite(ship.x, ship.y, "laser")
+        this.laser.setScale(0.1)
+        this.laser.setVelocityY(-700)
+        this.laser.setCollideWorldBounds(true)
+    }
+    if (this.laser && this.laser.y < 50) {
+        this.laser.disableBody(true, true);
+        this.laser.destroy(true)
+        this.laser = null
+        lasers -= 1;
+    }
 }
 
-function collectStar(player, star) {
-  star.disableBody(true, true);
-  score += 10;
-  scoreText.setText("Score: " + score);
-  if (stars.countActive(true) === 0) {
-    stars.children.iterate(function (child) {
-      child.enableBody(true, child.x, 0, true, true);
-    });
+const moveShip = () => {
+    if (cursors.left.isDown) {
+        ship.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        ship.setVelocityX(160);
+    } else {
+        ship.setVelocityX(0);
+    }
 
-    var x =
-      player.x < 400
-        ? Phaser.Math.Between(400, 800)
-        : Phaser.Math.Between(0, 400);
-
-    var bomb = bombs.create(x, 16, "bomb");
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-  }
+    if (cursors.down.isDown) {
+        ship.setVelocityY(160);
+    } else if (cursors.up.isDown) {
+        ship.setVelocityY(-160);
+    } else {
+        ship.setVelocityY(0);
+    }
 }
 
-function hitBomb(player, bomb) {
-  this.physics.pause();
+const shootLaser = () => {
 
-  player.setTint(0xff0000);
-
-  player.anims.play("turn");
-
-  gameOver = true;
 }
